91b6ace92d30c9a20a3f251f9ab10f98cbffc190
shauryr <shauryr@gmail.com>
2025-03-02 19:56:43 -0500

    added alien emoji

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,7 +46,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👽</text></svg>" />
       </head>
       <body className={`${inter.className} bg-gray-900 text-gray-100`}>{children}</body>
     </html>

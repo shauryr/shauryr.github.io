1297ec3e44f765269689a6aa5bf67581d8cdb99b
shauryr <shauryr@gmail.com>
2025-05-19 17:27:42 -0400

    Update package dependencies to Next.js version 14.2.28 and related packages, and set viewport meta tag in layout.tsx for improved responsiveness.

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,9 +34,10 @@ export const metadata: Metadata = {
     images: ['https://shaurya.ai/assets/profile.jpg'],
     creator: '@shauryr'
   },
-  viewport: 'width=device-width, initial-scale=1',
   robots: 'index, follow'
 }
+
+export const viewport = 'width=device-width, initial-scale=1'
 
 export default function RootLayout({
   children,
